f33038f11142dc41b84eb7ddaa759f19760a3cef
Timothy Lim <contacttimothylim@gmail.com>
2024-09-27 16:55:35 -0400

    commit

--- models/user.js
+++ models/user.js
@@ -1,5 +1,28 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  name: {
+     type: String,
+     required: true,
+   },
+   calories: {
+     type: Number,
+     required: true,
+   },
+   protein: {
+    type: Number,
+    required: true,
+   },
+   carbs: {
+    type: Number,
+    required: true,
+   },
+   fat: {
+    type: Number,
+    required: true,
+   },
+   });
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,7 +32,9 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-});
+  pantry: {
+    type: [foodSchema],
+}});
 
 const User = mongoose.model('User', userSchema);
 
